0c9fae77b5dd111ccedd82d183e8a5a8ce2ead67
Bruno Guedes <brunoguedesit@gmail.com>
2021-04-06 01:29:04 -0300

    feat(ci): travisci inclusion

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :stone_bank, StoneBank.Repo,
   username: "postgres",
   password: "postgres",
   database: "stone_bank_dev",
-  hostname: "database",
+  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
