881d44416ea311811f9e8535e1402ec94dc32831
Bruno Guedes <brunoguedesit@gmail.com>
2021-04-06 02:18:12 -0300

    mix format for passing build at travisci

--- config/releases.exs
+++ config/releases.exs
@@ -35,7 +35,7 @@ config :stone_bank, StoneBankWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-    config :stone_bank, StoneBankWeb.Endpoint, server: true
+config :stone_bank, StoneBankWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
