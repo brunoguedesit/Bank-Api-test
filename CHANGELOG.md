670cab02e00a84e43f24f3afbab895472d4046c2
Bruno Guedes <brunoguedesit@gmail.com>
2021-04-10 04:25:46 -0300

    refactor(operations) add gen transaction with Ecto.Multi

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,12 +2,11 @@
 
 ## Unreleasead
 
-## [0.0.20] - 2021-04-10
+## [0.0.21] - 2021-04-10
 ### Added
 
 ### Changed
 
-- refactor(transaction module): add a private function called filter_query to don't repeat yourself principle
-- refactor(transaction controller): clean our transaction module and put the asign of year and month into the year/month functions
+- refactor(operations): add a transaction with Ecto.Multi to insert the operations into transactions table
 
 ### Removed
--- lib/stone_bank/operations.ex
+++ lib/stone_bank/operations.ex
@@ -3,31 +3,43 @@ defmodule StoneBank.Operations do
     contains the operations of the bank
   """
 
+  @withdraw "withdraw"
+  @transfer "transfer"
+
   alias StoneBank.{Accounts, Accounts.Account}
   alias StoneBank.Repo
+  alias StoneBank.Transactions.Transaction
 
   def withdraw(f_id, value) do
     from = Accounts.get!(f_id)
     value = Money.new(:brl, value)
 
-    case is_negative_amount(from.amount, value) do
+    validate_negative(from.amount, value, withdraw_operation(from, value))
+  end
+
+  defp validate_negative(amount, value, operation) do
+    case is_negative_amount(amount, value) do
       true ->
         {:error, "you can't have negative amount!"}
 
       false ->
-        {:ok, from} = perform_operation(from, value, :sub) |> Repo.update()
-        {:ok, "Withdraw with success!!! from: #{from.id}, value: #{value}"}
+        operation
     end
+  end
+
+  defp withdraw_operation(from, value) do
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:account_from, perform_operation(from, value, :sub))
+    |> Ecto.Multi.insert(:transaction, gen_transaction(value, from.id, nil, @withdraw))
+    |> Repo.transaction()
+    |> transaction_case("Withdraw with success!!! from: #{from.id} value: #{value}")
   end
 
   def transfer(f_id, t_id, value) do
     from = Accounts.get!(f_id)
     value = Money.new(:brl, value)
 
-    case is_negative_amount(from.amount, value) do
-      true -> {:error, "you can't have negative amount!"}
-      false -> perform_update(from, t_id, value)
-    end
+    validate_negative(from.amount, value, perform_update(from, t_id, value))
   end
 
   defp is_negative_amount(from_amount, value) do
@@ -38,16 +50,20 @@ defmodule StoneBank.Operations do
   defp perform_update(from, t_id, value) do
     to = Accounts.get!(t_id)
 
-    transaction =
-      Ecto.Multi.new()
-      |> Ecto.Multi.update(:account_from, perform_operation(from, value, :sub))
-      |> Ecto.Multi.update(:account_to, perform_operation(to, value, :sum))
-      |> Repo.transaction()
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:account_from, perform_operation(from, value, :sub))
+    |> Ecto.Multi.update(:account_to, perform_operation(to, value, :sum))
+    |> Ecto.Multi.insert(:transaction, gen_transaction(value, from.id, to.id, @transfer))
+    |> Repo.transaction()
+    |> transaction_case("Transfer with success!!! from: #{from.id} to: #{to.id} value: #{value}")
+  end
 
-    case transaction do
-      {:ok, _} -> {:ok, "Transfer with sucess!! from: #{from.id} to: #{to.id} value: #{value}"}
+  defp transaction_case(operations, msg) do
+    case operations do
+      {:ok, _} -> {:ok, msg}
       {:error, :account_from, changeset, _} -> {:error, changeset}
       {:error, :account_to, changeset, _} -> {:error, changeset}
+      {:error, :transaction, changeset, _} -> {:error, changeset}
     end
   end
 
@@ -63,5 +79,15 @@ defmodule StoneBank.Operations do
 
   def update_account(%Account{} = account, params) do
     Account.changeset(account, params)
+  end
+
+  defp gen_transaction(value, f_id, t_id, type) do
+    %Transaction{
+      value: value,
+      account_from: f_id,
+      account_to: t_id,
+      type: type,
+      date: Date.utc_today()
+    }
   end
 end
